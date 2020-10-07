126cd28954d5f238f5d7fe9a3e496322843db35c
Yuri Trondin <lopata@2ch.hk>
2020-10-07 18:09:24 +0500

    initial commit

---
+++ src/components/post-list/post-list.js
@@ -0,0 +1,12 @@
+import React from "react";
+import PostListItem from "../post-list-item/";
+const PostList = () => {
+  return (
+    <ul className="app-list list-group">
+      <PostListItem />
+      <PostListItem />
+      <PostListItem />
+    </ul>
+  );
+};
+export default PostList;
---
+++ src/components/post-status-filter/post-status-filter.js
@@ -0,0 +1,14 @@
+import React from "react";
+const PostStatusFilter = () => {
+  return (
+    <div className="btn=group">
+      <button type="submit" className="btn btn-outline-secondary">
+        Все
+      </button>
+      <button type="submit" className="btn btn-outline-secondary">
+        Понравилось
+      </button>
+    </div>
+  );
+};
+export default PostStatusFilter
