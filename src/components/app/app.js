388b9928214c5f1210b3f7ac2d0a4f42f8f7f80f
Yuri Trondin <lopata@2ch.hk>
2020-10-11 21:47:05 +0500

    закончил приложение + поправил баг строки поиска, чтобы искалось без учета регистра

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -19,7 +19,6 @@ export default class App extends Component {
     super(props);
     this.state = {
       data: [
-        // { label: "Going to learn JS", important: false, id: "sda" },
         { label: "Going to learn React", important: true, like: false, id: 1 },
         { label: "Going to learn Vue", important: false, like: false, id: 2 },
         {
@@ -29,11 +28,15 @@ export default class App extends Component {
           id: 3,
         },
       ],
+      term: "",
+      filter: "all",
     };
     this.deleteItem = this.deleteItem.bind(this);
     this.addItem = this.addItem.bind(this);
     this.onToggleImportant = this.onToggleImportant.bind(this);
     this.onToggleLiked = this.onToggleLiked.bind(this);
+    this.onUpdateSearch = this.onUpdateSearch.bind(this);
+    this.onFilterSelect = this.onFilterSelect.bind(this);
     this.maxId = 4;
   }
 
@@ -64,39 +67,80 @@ export default class App extends Component {
   }
 
   onToggleImportant(id) {
-    // console.log(`important ${id}`)
+    this.setState(({ data }) => {
+      const index = data.findIndex((elem) => elem.id === id);
+
+      const old = data[index];
+      const newItem = { ...old, important: !old.important };
+
+      const newArr = [
+        ...data.slice(0, index),
+        newItem,
+        ...data.slice(index + 1),
+      ];
+      return {
+        data: newArr,
+      };
+    });
   }
 
- onToggleLiked(id) {
-  // console.log(`likw ${id}`)
+  onToggleLiked(id) {
+    this.setState(({ data }) => {
+      const index = data.findIndex((elem) => elem.id === id);
 
-        this.setState(({data}) => {
-            const index = data.findIndex(elem => elem.id === id);
+      const old = data[index];
+      const newItem = { ...old, like: !old.like };
 
-            const old = data[index];
-            const newItem = {...old, like: !old.like};
+      const newArr = [
+        ...data.slice(0, index),
+        newItem,
+        ...data.slice(index + 1),
+      ];
+      return {
+        data: newArr,
+      };
+    });
+  }
 
-            const newArr = [...data.slice(0, index), newItem, ...data.slice(index + 1)];
-            return {
-                data: newArr
-            }
-        }); 
+  filterPost(items, filter) {
+    if (filter === "like") {
+      return items.filter((item) => item.like);
+    } else {
+      return items;
     }
-
+  }
+  searchPost(items, term) {
+    if (term.length === 0) {
+      return items;
+    }
+    return items.filter((item) => {
+      return item.label.toLowerCase().indexOf(term) > -1;
+    });
+  }
+  onUpdateSearch(term) {
+    this.setState({ term });
+  }
+  onFilterSelect(filter) {
+    this.setState({ filter });
+  }
   render() {
-    const { data } = this.state;
+    const { data, term, filter } = this.state;
     const liked = data.filter((item) => item.like).length;
     const allPosts = data.length;
+    const visiblePosts = this.filterPost(this.searchPost(data, term), filter);
 
     return (
       <AppBlock>
         <AppHeader liked={liked} allPosts={allPosts} />
         <div className="search-panel d-flex">
-          <SearchPanel />
-          <PostStatusFilter />
+          <SearchPanel onUpdateSearch={this.onUpdateSearch} />
+          <PostStatusFilter
+            filter={filter}
+            onFilterSelect={this.onFilterSelect}
+          />
         </div>
         <PostList
-          posts={this.state.data}
+          posts={visiblePosts}
           onDelete={this.deleteItem}
           onToggleImportant={this.onToggleImportant}
           onToggleLiked={this.onToggleLiked}
--- src/components/post-add-form/post-add-form.js
+++ src/components/post-add-form/post-add-form.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { Component } from "react";
 // import "./post-add-form.css";
 import styled from "styled-components";
-const BottomPanel = styled.div`
+const BottomPanel = styled.form`
   display: flex;
   margin-top: 20px;
   bottom-panel .new-post-label {
@@ -11,22 +11,43 @@ const BottomPanel = styled.div`
   }
 `;
 
-const PostAddForm = ({ onAdd }) => {
-  return (
-    <BottomPanel>
-      <input
-        type="text"
-        placeholder="Что хорошего произошло?"
-        className="form-control new-post-label"
-      />
-      <button
-        type="submit"
-        className="btn btn-outline-secondary"
-        onClick={() => onAdd("Hello")}
-      >
-        Добавить
-      </button>
-    </BottomPanel>
-  );
-};
-export default PostAddForm;
+export default class PostAddForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: "",
+    };
+    this.onValueChange = this.onValueChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+  onValueChange(e) {
+    this.setState({
+      text: e.target.value,
+    });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    this.props.onAdd(this.state.text);
+    this.setState({
+      text: "",
+    });
+  }
+
+  render() {
+    return (
+      <BottomPanel onSubmit={this.onSubmit}>
+        <input
+          type="text"
+          placeholder="Что хорошего произошло?"
+          className="form-control new-post-label"
+          onChange={this.onValueChange}
+          value={this.state.text}
+        />
+        <button type="submit" className="btn btn-outline-secondary">
+          Добавить
+        </button>
+      </BottomPanel>
+    );
+  }
+}
